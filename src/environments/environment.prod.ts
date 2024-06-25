ab8bdb0359440d424ce70cf6004e9aae036cfc24
mdsjl**2023 <desarrollo@munisjl.gob.pe>
2024-06-25 16:27:33 -0500

    app-noticias-ionic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:'9e9d7618403d4b368168203d5c165f44'
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'9e9d7618403d4b368168203d5c165f44'
 };
 
 /*
