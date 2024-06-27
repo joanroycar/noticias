858c348a29e2d3ddeb9e7d1a0ac4106fd8c60a1a
Joan Casa Nueva PC <joanroycar@gmail.com>
2024-06-26 23:17:53 -0500

    UPDATE SHARE APP SOCIAL

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,33 +1,64 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from '../../interfaces';
 import { Browser } from '@capacitor/browser';
+import { ActionSheetController } from '@ionic/angular';
+
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit { 
-
+export class ArticleComponent implements OnInit {
   @Input() article: Article;
 
   @Input() index: number;
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController) {}
 
-  ngOnInit() {
-
-
-
-  }
-  openArticle(){
-    
-    Browser.open({url:this.article.url });
-
+  ngOnInit() {}
+  openArticle() {
+    Browser.open({ url: this.article.url });
   }
 
+  async onOpenMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Opciones',
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share-outline',
+          handler: () => this.onShareArticle(),
+        },
+        {
+          text: 'Favorito',
+          icon: 'heart-outline',
+          handler: () => this.onToggleFavorite(),
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close-outline',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+      ],
+    });
 
-  onClick(){
-    
+    await actionSheet.present();
   }
+
+  async onShareArticle() {
+    // console.log('FAVORITE CLICKED');
+
+
+    await Share.share({
+      title: this.article.title,
+      text: this.article.description,
+      url: this.article.url,
+      dialogTitle: this.article.title,
+    });
+  }
+
+  onToggleFavorite() {}
 }
